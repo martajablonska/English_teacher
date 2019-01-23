02fc0cdbd9bb3422198c216941367b40fd2081b5
martajablonska <martawaldon@gmail.com>
2019-01-23 13:36:37 +0100

    Added anchors to menu

---
+++ js/scroll.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  
+let width= window.innerWidth;    
+        
+    function scroll(e) {
+
+    let href = $(this).attr('href');
+    e.preventDefault();
+
+    if(width < 950) {                     
+       $('html, body').animate({
+            scrollTop: $(href).offset().top - 40
+        }, 800); 
+    } else {                        
+        $('html, body').animate({
+            scrollTop: $(href).offset().top - 110
+        }, 800);  
+    };
+
+    location.hash = href;
+    };
+
+    $('a[href*="#"]').click(scroll);
+    
+});
